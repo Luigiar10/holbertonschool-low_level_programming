f0ae9abf7d27ef972bfb12f330ccebd1f725393f
Luigi <luigi10arrieta@gmail.com>
2020-09-19 02:33:08 +0000

    exclusion

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+	char q = 'q';
+	char e = 'e';
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+		if(alpha != q && alpha != e)
+		{
+			putchar(alpha);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
