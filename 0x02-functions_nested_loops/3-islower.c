f4ad46a2423c00a38ac39f9ada1b0c5c01dba75c
Luigi <luigi10arrieta@gmail.com>
2020-09-22 23:35:19 +0000

    islower check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * islower - Entry point
+  *
+  * Return: 0 and 1
+  */
+int _islower(int c)
+{
+	if( (c >= 'a' && c <= 'z') )
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
