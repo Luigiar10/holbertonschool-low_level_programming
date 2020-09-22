a14132552fbd1282e5429bfbf994da909788a124
Luigi <luigi10arrieta@gmail.com>
2020-09-22 22:41:12 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,16 @@
 /**
   * print_alphabet 
   *
-  * Return: Always 0.
+  * Return: Always 0
   */
 
 void print_alphabet(void)
 {
 	int alphabet;
-	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
 	}
 	_putchar('\n');
 
-	return(0);
 }
