1de0e6ab5ca02cd49489afd5c4684e55f71beaf5
Luigi <luigi10arrieta@gmail.com>
2020-09-23 00:14:32 +0000

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,12 @@
 #include "holberton.h"
-#include <ctype.h>
 /**
  * _isalpha - Entry point
- *
- * Return: Always 0 
+ * @c: check
+ * Return: 1
  */
 int _isalpha(int c)
 {
-
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
-	{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
-	}
-	else
-	{
 		return (0);
-	}
 }
