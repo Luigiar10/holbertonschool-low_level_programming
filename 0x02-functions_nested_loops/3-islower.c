84a65c05973c999054d083c9cf9565cc9dacbfd5
Luigi <luigi10arrieta@gmail.com>
2020-09-23 00:08:52 +0000

    islower checker

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
 /**
   * _isalpha - Entry point
-  *@c: checker
-  * Return: 1 
+  * @c: checker
+  * Return: 1 and 0
   */
 int _isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-		return (1);
-		return (0);
+	{
+		return(1);
+	}else
+	{
+		return(0);
+	}
 }
