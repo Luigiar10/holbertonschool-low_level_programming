54ba3f311c5bddc38386705e626afafb12ca8f0b
Luigi <luigi10arrieta@gmail.com>
2020-09-24 01:23:20 +0000

    Add integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+  * add - Entry point
+  * @a: variable input
+  * @b: variable input
+  * Return: the add.
+  */
+int add(int int1, int int2)
+{
+	int result;
+	result = int1 + int2;
+	return (result);
+}
