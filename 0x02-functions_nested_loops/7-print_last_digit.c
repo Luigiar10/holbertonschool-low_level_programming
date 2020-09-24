660ce6139e7e2e501e0ea3d82b0a06272a7d8ebb
Luigi <luigi10arrieta@gmail.com>
2020-09-24 02:14:30 +0000

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,18 +8,17 @@
 
 int print_last_digit(int ram)
 {
-	int num = ram % 10;
+int num = ram % 10;
 
-	if (num >= 0)
-	{
-		_putchar('0' + num);
-		return (num);
-	}
-	else
-	{
-		num = num * (-1);
-		_putchar('0' + num);
-		return (num);
-	}
-	return(0);
+if (num >= 0)
+{
+_putchar('0' + num);
+return (num);
+}
+else
+{
+num = num * (-1);
+_putchar('0' + num);
+return (num);
+}
 }
