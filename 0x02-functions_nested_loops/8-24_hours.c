36399fd477eac12cba3343e9e212d07cb5f92eab
Luigi <luigi10arrieta@gmail.com>
2020-09-24 02:18:59 +0000

    Clock

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,20 +7,19 @@
   */
 void jack_bauer(void)
 {
-	int hours;
-	int minutes;
+int hours;
+int minutes;
 
-	for (hours = 0; hours <= 23; hours++)
-	{
-		for (minutes = 0; minutes <= 59; minutes++)
-		{
-			_putchar( (hours / 10) + '0');
-			_putchar( (hours % 10) + '0');
-			_putchar(':');
-			_putchar('0' + (minutes / 10) );
-			_putchar('0' + (minutes % 10) );
-			_putchar('\n');
-		}
-	}
-	return(0);
+for (hours = 0; hours <= 23; hours++)
+{
+for (minutes = 0; minutes <= 59; minutes++)
+{
+_putchar( (hours / 10) + '0');
+_putchar( (hours % 10) + '0');
+_putchar(':');
+_putchar('0' + (minutes / 10) );
+_putchar('0' + (minutes % 10) );
+_putchar('\n');
+}
+}
 }
