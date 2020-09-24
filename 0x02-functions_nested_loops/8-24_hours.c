e037d38900ff547913242b866131d0be881e04f7
Luigi <luigi10arrieta@gmail.com>
2020-09-24 00:55:16 +0000

    Clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  * jack_bauer - Entry point
+  * variables of data type int
+  * Return: Always 0 (Success)
+  *
+  */
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar( (hours / 10) + '0');
+			_putchar( (hours % 10) + '0');
+			_putchar(':');
+			_putchar('0' + (minutes / 10) );
+			_putchar('0' + (minutes % 10) );
+			_putchar('\n');
+		}
+	}
+}
