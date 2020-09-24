fc9b35564ea2c300ce7ded8a2bc0cf0a40dad7e9
Luigi <luigi10arrieta@gmail.com>
2020-09-24 00:40:55 +0000

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,23 @@
 #include "holberton.h"
 /**
-  * print_last_digit - Entry point 
-  * ram: provider ramdon num
-  * Return: always 0
+  * print_last_digit - Entry point
+  * ram: provider ramdom num
+  * Return: Always 0 
   */
 int print_last_digit(int ram)
 {
 	int num = ram % 10;
 
-	if(num >= 0)
+	if (num >= 0)
 	{
 		_putchar('0' + num);
-		return(num);
+		return (num);
 	}
 	else
 	{
 		num = num * (-1);
 		_putchar('0' + num);
-		return(num);
+		return (num);
 	}
-
 	return(0);
 }
