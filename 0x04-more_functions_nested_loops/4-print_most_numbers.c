b780e97a1bfba53f7fa7d9dfb3af44437384bd05
Luigi <luigi10arrieta@gmail.com>
2020-10-01 19:28:13 +0000

    Number exception

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
 /**
-  * print_most_numbers - this function print numbers.
+  * print_most_numbers - print numbers
+  *
   * @num: variable of type int 
+  *
   * Return: new line
   */
 void print_most_numbers(void)
 {
 	int num;
+
 	for (num = '0'; num <= '9'; num++)
 	{
 		if ( (num != '2') && (num != '4') )
