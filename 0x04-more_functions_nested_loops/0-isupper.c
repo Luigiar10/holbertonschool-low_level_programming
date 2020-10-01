105a6a66a80f6865e62575d9add8de42d745337c
Luigi <luigi10arrieta@gmail.com>
2020-10-01 18:44:58 +0000

    Check Alphabet

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * _isupper - check alphabet
+  * @c: variable
+  * Return: 1 
+  */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
