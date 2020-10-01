d91c75967406ac497774747e8246314849435abd
Luigi <luigi10arrieta@gmail.com>
2020-10-01 19:18:53 +0000

    Numbers 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,13 @@
 #include "holberton.h"
 /**
   * print_numbers - this function print numbers.
+  *
   * Return: new line
   */
 void print_numbers(void)
 {
 	int num;
+
 	for (num = '0'; num <= '9'; num++)
 	{
 		_putchar(num);
