9eaf172fcdc9e0454b09e0d01de4a31604c10d7c
Luigi <luigi10arrieta@gmail.com>
2020-10-01 19:53:37 +0000

    Numbers 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+  * more_numbers - this function print more numbers.
+  *
+  *
+  */
+void more_numbers(void)
+{
+	int ten, number;
+
+	for (ten = 1; ten <= 10; ten++)
+	{
+		for (number = 10; number <= 24 ; number++)
+		{
+			if (number > 19)
+			{
+				_putchar( (number / 20) + '0');
+			}
+			else
+			{
+			}
+			_putchar( (number % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
