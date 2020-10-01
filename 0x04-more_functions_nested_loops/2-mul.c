22871bb374602183fd29b9d0b4fe2428dbd28f9a
Luigi <luigi10arrieta@gmail.com>
2020-10-01 18:57:52 +0000

    Multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * mul - function that multiplies to integers.
+  * @a: input variable.
+  * @b: input variable too.
+  * Return: the solution.
+  */
+int mul(int a, int b)
+{
+	int multi;
+
+	multi = a * b;
+
+	return (multi);
+}
