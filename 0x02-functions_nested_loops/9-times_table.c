101ea74f95349e9f279ac6db80ac4c57355c255c
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-10-15 10:36:10 -0500

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,38 +5,37 @@
  */
 void times_table(void)
 {
-	int hor, ver;
-	
-	for (hor = 0; hor <= 9; hor++)
-	{
-		for (ver = 0; ver <= 9; ver++)
-		{
-			if (hor * ver >= 10)
-			{
-				_putchar((hor * ver) / 10 + '0');
-              			_putchar((hor * ver) % 10 + '0');
 
-                     		if (ver != 9)
-				{
-                     			_putchar(',');
-                          		_putchar(' ');
-                     		}
-			}
-                	else
-			{
-                		if (ver != 0)
-				{
-                        		_putchar(' ');
-                        	}
-                        	_putchar((hor * ver) % 10 + '0');
-                        	if (ver != 9)
-				{
-                    			_putchar(',');
-                           		_putchar(' ');
-                        	}
-			}
-       		}
-       		_putchar('\n');
-       }
+int hor, ver;
 
+for (hor = 0; hor <= 9; hor++)
+{
+for (ver = 0; ver <= 9; ver++)
+{
+if (hor * ver >= 10)
+{
+_putchar((hor * ver) / 10 + '0');
+_putchar((hor * ver) % 10 + '0');
+if (ver != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+else
+{
+if (ver != 0)
+{
+_putchar(' ');
+}
+_putchar((hor * ver) % 10 + '0');
+if (ver != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
+}
 }
