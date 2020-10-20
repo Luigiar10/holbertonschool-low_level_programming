83a00496cf9182073a12cf834c5b109f5005083c
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-10-20 08:52:34 -0500

    Structure Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * DOG - name of struct and 
+  * @name: variable
+  * @age: variable
+  * @owner: variable
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
