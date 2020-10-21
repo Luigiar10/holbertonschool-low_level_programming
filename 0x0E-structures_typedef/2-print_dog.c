59f40575792e15897e3dc02196081c98cf62889e
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-10-20 19:36:59 -0500

    Structure dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include "dog.h"
 /**
-  * print_dog: entry point
+  * print_dog - entry point and provider of string
   *
-  *
-  *
+  * @d: variable
   */
 void print_dog(struct dog *d)
 {
-	if(d != NULL)
+	if (d != NULL)
 	{
-		if(d->name == NULL)
+		if (d->name == NULL)
 		{
 			printf("Name: (nil)\n");
 		}
 		else
 		{
-		  	printf("Name: %s\n", d->name);
+			printf("Name: %s\n", d->name);
 		}
-		printf("Age: %f\n", d->age);
-		if(d->owner == NULL)
+			printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
 		{
-		  	printf("Owner: (nil)\n");
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
-		  	printf("Owner: %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 		}
 	}
 }
