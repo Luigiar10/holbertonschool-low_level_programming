e1b9fd42fbb98b12eecd2f9d458adee9ed550bcd
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-10-20 19:06:51 -0500

    Init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+  * init_dog - entry point
+  *
+  * @d: variable
+  * @name: variable
+  * @age: variable
+  * @owner: variable
+  *
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
