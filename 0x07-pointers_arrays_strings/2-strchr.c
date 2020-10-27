1cb55b17b1c96283390bf3f3a992cc66e9c87dbc
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-10-27 16:00:07 -0500

    Strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strchr - entry point
+ *
+ * @s: variable
+ * @c: variable
+ *
+ * Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+	do
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	while (*s++);
+	return (0);
+}
