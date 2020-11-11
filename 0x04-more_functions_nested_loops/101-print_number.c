bf2d094dd1d0a3b782dd1a5e9ba2397c2fcbc9c5
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-11 06:17:38 -0500

    Print Number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * print_number - entry point
+ *
+ * @n: variable.
+ *
+ */
+void print_number(int n)
+{
+	if (n > 1000)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		if (n > 100)
+		{
+			_putchar((n / 100) + '0');
+			_putchar(((n / 10) % 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		else
+		{
+			if (n > 10)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else
+			{
+				if (n == 0)
+				{
+					_putchar ('0');
+				}
+				else
+				{
+					if (n < 0)
+					{
+						_putchar('-');
+						_putchar(((n * -1) / 10) + '0');
+						_putchar(((n * -1) % 10) + '0');
+					}
+				}
+			}
+		}
+	}
+}
