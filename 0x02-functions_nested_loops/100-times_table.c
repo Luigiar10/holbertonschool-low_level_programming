186d64fa8dfd4f79c7552caae9d982f8903a0c1f
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-11 05:45:35 -0500

    Print n Numbers

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+/**
+ * print_times_table - this make a times table.
+ * @n: variable input.
+ */
+void print_times_table(int n)
+{
+if (n >= 0 && n <= 15)
+{
+int x, z, y;
+for (x = 0; x <= n; x++)
+{
+for (z = 0; z <= n; z++)
+{
+y = x * z;
+if (z == 0)
+{
+_putchar(y + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+if (y / 100 == 0)
+{
+_putchar(' ');
+if (y / 10 == 0)
+{
+_putchar(' ');
+_putchar(y + '0');
+}
+else
+{
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+}
+}
+else
+{
+_putchar((y / 100) + '0');
+_putchar(((y / 10)) % 10 + '0');
+_putchar((y % 10) + '0');
+}
+}
+}
+_putchar('\n');
+}
+}
+else
+{
+}
+}
