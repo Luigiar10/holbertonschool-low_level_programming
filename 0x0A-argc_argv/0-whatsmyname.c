bde8b7c9f9e154eac0b473e7d7582f1034fe05ec
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-11 20:05:29 -0500

    My name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
