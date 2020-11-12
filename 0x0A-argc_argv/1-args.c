4d39bab3af7dabafcc44021a8108b16bd50589e2
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-11 20:11:20 -0500

    Nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * 
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	char x;
+
+	x = *argv[0];
+	x = x + 1;
+	printf("%d\n", argc - 1);
+	return (0);
+}
