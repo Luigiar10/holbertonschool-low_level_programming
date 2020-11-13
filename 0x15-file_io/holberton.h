34887ca86a664f6f3249b3542006fefe78533107
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-13 17:42:51 -0500

    Prototypes

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *, size_t);
+int create_file(const char *, char *);
+int append_text_to_file(const char *, char *);
+
+#endif
