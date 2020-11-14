0253c8cf40b64b614f62860868a7b6c65bae4b39
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-14 14:28:17 -0500

    Structure and Prototypes

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,23 @@
+#ifndef VARFUNC_H
+#define VARFUNC_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int, ...);
+void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+/**
+ * struct print - struct
+ * @spec: the specification.
+ * @func: the function.
+ */
+typedef struct print
+{
+	char *spec;
+	void (*func)(va_list);
+} print_data;
+
+#endif
