d04dcc370276f87ceddb5d760863d507a1d0f35c
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-16 14:39:05 -0500

    Calc

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <string.h>
+/**
+* get_op_func - entry point
+*
+* @s: operation
+*
+* Return: ops or functions
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int x;
+
+	x = 0;
+	while (ops[x].op != NULL)
+	{
+		if (strcmp(s, ops[x].op) == 0)
+		{
+			break;
+		}	
+		x++;
+	}
+	return (ops[x].f);
+}
