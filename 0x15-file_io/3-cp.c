37409a8637d472ec85f0ff42ea0dc5e1ec612c61
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-16 14:19:35 -0500

    Copy from to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,122 +1,116 @@
 #include "holberton.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-int copy_file(char *file_from, char *file_to);
+#include <unistd.h>
+#include <fcntl.h>
+
+void error_file_from(char *, char *);
+void error_file_to(char *, char *);
+void error_close(int, char *);
+
 /**
- * main - entry point
- *
- * @ac: arguments
- * @av: arguments
- *
- * Return: Always 0
- */
-int main(int ac, char **av)
+* copy_file - entry point
+*
+* @file_to: Arguments
+* @file_from: Arguments
+*
+* Return: Always 0
+*/
+int copy_file(char *file_to, char *file_from)
 {
-	int max;
+	int hac, desd, x, er, ma;
+	char *max;
 
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+	max = malloc(sizeof(char) * 1024);
+	if (!max)
+		return (0);
 
-	max = copy_file(av[1], av[2]);
+	desd = open(file_from, O_RDONLY);
+	if (desd == -1)
+		error_file_from(file_from, max);
 
-		if (max == 98)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
-		else if (max == 99)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
+	hac = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (hac == -1)
+		error_file_to(file_to, max);
 
+	ma = read(desd, max, 1024);
+
+	do {
+	if (ma == -1)
+		error_file_from(file_from, max);
+	if (ma == 0)
+		break;
+	x = write(hac, max, ma);
+	if (x == -1)
+		error_file_to(file_to, max);
+	ma = read(desd, max, 1024);
+	} while (ma > 0);
+
+	er = close(hac);
+	if (er == -1)
+		error_close(hac, max);
+	er = close(desd);
+	if (er == -1)
+		error_close(desd, max);
+
+	free(max);
 	return (0);
 }
 
 /**
- * copy_file - entry point
- *
- * @file_from: arguments
- * @file_to: arguments
- *
- * Return: exit
- */
-int copy_file(char *file_from, char *file_to)
+* error_close - error closing
+*
+* @fd: arguments
+* @max: arguments
+*/
+void error_close(int fd, char *max)
 {
-	int de, ac, x, z, l;
-	char *copiar;
+	free(max);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
 
-	if (!file_from)
+/**
+* error_file_from - error file
+
+* @file_from: arguments
+* @max: arguments
+*/
+void error_file_from(char *file_from, char *max)
+{
+	free(max);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	exit(98);
+}
+
+/**
+* error_file_to - error created
+
+* @file_to: arguments
+* @max: arguments
+*/
+void error_file_to(char *file_to, char *max)
+{
+	free(max);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	exit(99);
+}
+
+/**
+* main - main of our program
+*
+* @argc: arguments
+* @argv: arguments
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
 	{
-		return (98);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-
-	de = open(file_from, O_RDONLY);
-		if (de == -1)
-		{
-			return (98);
-		}
-
-	copiar = malloc(sizeof(char) * BUF);
-		if (!copiar)
-		{
-			return (98);
-		}
-
-	ac = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-		if (ac == -1)
-		{
-			free(copiar);
-			return (99);
-		}
-
-	x = read(de, copiar, BUF);
-		if (x == -1)
-		{
-			free(copiar);
-			return (98);
-		}
-
-	while (x != 0)
-	{
-		z = write(ac, copiar, x);
-			if (z == -1 || z != x)
-			{
-				free(copiar);
-				return (99);
-			}
-
-		x = read(de, copiar, BUF);
-				if (x == -1)
-				{
-					free(copiar);
-					return (98);
-				}
-	}
-
-	free(copiar);
-
-	l = close(de);
-	if (l < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", de);
-		exit(100);
-	}
-
-	if (close(ac) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ac);
-		exit(100);
-	}
-
-	exit(0);
+	copy_file(argv[2], argv[1]);
+	return (0);
 }
