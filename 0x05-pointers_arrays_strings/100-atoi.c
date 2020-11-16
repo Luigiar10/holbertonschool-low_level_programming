7f3a020b41c813eb5c99c0678687c0465e8a245c
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-16 11:50:30 -0500

    Atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+* _atoi - entry point
+*
+* @s: string
+*
+* Return: n
+*/
+int _atoi(char *s)
+{
+	int x;
+	int z = 0; 
+	int n = 0;
+	int j = 1;
+
+	while ((*(s + z) < '0' || *(s + z) > '9') && (*(s + z) != '\0'))
+	{
+		if (*(s + z) == '-')
+			j *= -1;
+		z++;
+	}
+	x = z;
+	while ((*(s + x) >= '0') && (*(s + x) <= '9'))
+	{
+		n = n * 10 + j * (*(s + x) - '0');
+		x++;
+	}
+	return (n);
+}
