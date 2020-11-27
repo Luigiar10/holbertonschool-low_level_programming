3be7e6f606e9da5754d095b79544232ad440a9e0
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-27 18:12:02 -0500

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 /**
- * print_binary - this function prints the binary number of a decimal.
- * @n: the decimal number.
+ * print_binary - entry point
+ * 
+ * @n: number.
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned long int a;
-	int b;
+	unsigned long int x;
+	int z;
 
 	if (n == 0)
 	{
 		_putchar('0');
 	}
-	for (b = ((sizeof(unsigned long int) * 8) - 1); b >= 0; b--)
+	for (z = ((sizeof(unsigned long int) * 8) - 1); z >= 0; z--)
 	{
-		a = n >> b;
-		if (a)
+		x = n >> z;
+		if (x)
 		{
-			if (a & 1)
+			if (x & 1)
 				_putchar('1');
 			else
 				_putchar('0');
