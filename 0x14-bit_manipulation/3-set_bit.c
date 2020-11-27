34ef013352b78f31cd3d2d488488e1e1b546954e
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-27 18:23:09 -0500

    Set Bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -15,9 +15,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	srch <<= index;
 	if (index > (sizeof(unsigned long int) * 8) - 1)
-	{
 		return (-1);
-	}
 	*n = *n | srch;
 	return (1);
 }
