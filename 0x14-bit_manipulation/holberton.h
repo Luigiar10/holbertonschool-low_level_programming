2105627b59c3cc4bd11b30a3c551850dfa909c5e
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-27 18:05:57 -0500

    Prototipos

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,1 +1,13 @@
-unsigned int binary_to_uint(const char *b);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+unsigned int binary_to_uint(const char *);
+void print_binary(unsigned long int);
+int _putchar(char);
+int get_bit(unsigned long int, unsigned int);
+int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif
