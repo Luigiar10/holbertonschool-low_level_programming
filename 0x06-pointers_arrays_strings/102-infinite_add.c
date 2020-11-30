bea30f5dbb423feb8f0b0fc5d54e98bdabe714f2
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-11-30 10:38:35 -0500

    Fix Advanced

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,84 +1,41 @@
-#define FILL 100 /* fill with 'd' */
-void move_int_end_to_beg(char *str, int size, char fill);
 /**
-* infinite_add - adds two numbers
+* infinite_add - entry point
 *
-* @n1: *char - first number
-* @n2: *char - second number
-* @r:  *char - buffer where result is stored
-* @size_r: int - size of buffer
+* @n1: arguments
+* @n2: arguments
+* @r: arguments
+* @n: arguments
 *
-* Return: pointer to r
+* Return: Pointer
 */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int n)
 {
-int tempSizeR = size_r;
-int n1Length = 0, n2Length = 0, result = 0, carryStore = 0;
-while (*(n1 + n1Length) != '\0')
-n1Length++;
-while (*(n2 + n2Length) != '\0')
-n2Length++;
-/* account for 9999, 1, size_r:4, and extra space needed for '\0' */
-if (n1Length > size_r - 2 || n2Length > size_r - 2)
-return (0);
-n1Length--, n2Length--; /* set element behind null byte */
-while (size_r >= 0)
-{
-if (size_r == tempSizeR)
-{ /* set last byte to end of string char or 'null byte' */
-r[size_r--] = '\0';
-continue;
-}
-if (n1Length < 0 && n2Length < 0 && carryStore == 0)
-{
-r[size_r--] = FILL;
-continue; /* fill in rest with constants's */
-}
-else if (n1Length < 0 && n2Length < 0 && carryStore != 0)
-result = carryStore;
-else if (n1Length < 0)
-result = n2[n2Length] + carryStore - '0';
-else if (n2Length < 0)
-result = n1[n1Length] + carryStore - '0';
-else /* normal case */
-{
-result = (n1[n1Length] - '0') + (n2[n2Length] - '0') + carryStore;
-}
-carryStore = 0;
-if (result > 9)
-{
-carryStore = 1;
-result = result - 10;
-}
-n1Length--, n2Length--;
-r[size_r--] = result + '0';
-}
-move_int_end_to_beg(r, tempSizeR, FILL);
-return (r);
-}
-/**
-* move_int_end_to_beg - moves integer from end of str to beginning
-*
-* @arr: string to change
-* @arrSize: size of string including null byte
-* @fillChar: character used to fill string that isn't part being moved
-*
-* Return: always void
-*/
-void move_int_end_to_beg(char *arr, int arrSize, char fillChar)
-{
-int i = 0;
-int bufferPlacer = 0;
-/* below loop moves elements to beginning of array */
-while (i < arrSize)
-{
-if (arr[i] != fillChar)
-{
-arr[bufferPlacer++] = arr[i];
-arr[i] = '\0';
-}
-else
-arr[i] = '\0';
-i++;
-}
+	int tam1 = 0, tam2 = 0;
+	int max = 0;
+	int x = n - 2;
+
+	while (n1[tam1 + 1] != 0)
+		tam1++;
+	while (n2[tam2 + 1] != 0)
+		tam2++;
+	r[n - 1] = 0;
+
+	while (x >= 0 && (tam1 >= 0 || tam2 >= 0))
+	{
+		max += (tam1 < 0 ? '0' : n1[tam1]) + (tam2 < 0 ? '0' : n2[tam2]);
+		max -= 2 * '0';
+		r[x] = max % 10 + '0';
+		max /= 10;
+		x--;
+		tam1--;
+		tam2--;
+	}
+
+	if ((x < tam1 || x < tam2) || (x < 0 && max))
+	return (0);
+
+	max ? r[x] = max + '0' : 1;
+	x += max ? 0 : 1;
+
+	return (r + x);
 }
