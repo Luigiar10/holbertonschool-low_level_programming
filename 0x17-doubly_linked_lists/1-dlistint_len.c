9aa25d026882025a75d96c04a69d89bfd1ef3a78
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-12-14 15:52:46 -0500

    Add List

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - entry point
+ * @h: list
+ * Return: elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int x = 0;
+
+	for (x = 0; h != NULL; x++)
+	{
+		h = h->next;
+	}
+	return (x);
+}
