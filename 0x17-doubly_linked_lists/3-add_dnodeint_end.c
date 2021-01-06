5281ee166ef2274d63fc6c8a15aac657e9536c59
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-01-06 23:21:42 +0000

    Fix Identation | Add Node | Luigi A

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,26 +9,26 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *newnode, *tmp;
-    int x;
+	dlistint_t *newnode, *tmp;
+	int a;
 
-    newnode = malloc(sizeof(dlistint_t));
-    if (newnode == NULL)
-        return (NULL);
-    newnode->n = n;
-    newnode->next = NULL;
-    if (*head == NULL)
-    {
-        newnode->prev = NULL;
-        *head = newnode;
-        return (newnode);
-    }
-    tmp = *head;
-    for (x = 0; tmp->next != NULL; x++)
-    {
-        tmp = tmp->next;
-    }
-    tmp->next = newnode;
-    newnode->prev = tmp;
-    return (newnode);
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = NULL;
+	if (*head == NULL)
+	{
+		newnode->prev = NULL;
+		*head = newnode;
+		return (newnode);
+	}
+	tmp = *head;
+	for (a = 0; tmp->next != NULL; a++)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = newnode;
+	newnode->prev = tmp;
+	return (newnode);
 }
